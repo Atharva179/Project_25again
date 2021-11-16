f252fb8e97d4a34157b19789e084a9ea3840e88e
Atharva179 <79782793+Atharva179@users.noreply.github.com>
2021-11-16 14:14:47 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,29 @@
+class Paper{
+    constructor(x,y,r){
+        var options={
+			isStatic:false,
+            restitution:0.3,
+            density:1.2,
+            friction:0			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		
+		this.body=Bodies.circle(this.x,this.y,(this.r-20)/2, options);
+        this.image=loadImage("paper.png")
+ 		World.add(world, this.body);
+    }
+
+    display(){
+        var paperpos = this.body.position
+        var angle = this.body.angle;
+
+        push();
+        translate(paperpos.x ,paperpos.y);
+        rotate(angle)
+        imageMode(CENTER)
+        image(this.image,0,0,this.r,this.r)
+        pop();
+    }
+}
